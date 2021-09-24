c7201e5a7bba792f57583f1ff9ac2896bd6856b6
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-09-24 17:17:05 +0300

    Add CanonController.cs: Canon move logic

---
+++ Assets/Scripts/CanonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CanonController : MonoBehaviour
+{
+    [SerializeField] CanonControllerData canonData;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        float canonPosX = transform.position.x + canonData.MoveSpeed * horizontalInput * Time.deltaTime;
+        canonPosX = Mathf.Clamp(canonPosX, canonData.LowerThreshold, canonData.UpperThreshold);
+        transform.position = new Vector3(canonPosX, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/CanonControllerData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Canon Controller Data", menuName = "Mob Crowd/Canon Controller Data")]
+public class CanonControllerData : ScriptableObject
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float lowerThreshold;
+    [SerializeField] float upperThreshold;
+
+    public float MoveSpeed { get => moveSpeed; }
+    public float LowerThreshold { get => lowerThreshold; }
+    public float UpperThreshold { get => upperThreshold; }
+}
