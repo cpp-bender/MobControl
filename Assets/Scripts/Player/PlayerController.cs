8b34d596932274fbbf6f0279fd5719ead1fd2907
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-09-26 14:52:01 +0300

    Add Obstacle prefab, Update PlayerController.cs: Collision with obstacle logic

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] PlayerData playerData;
 
     private GameObject destination;
+    private Rigidbody body;
 
     private void Awake()
     {
+        body = GetComponent<Rigidbody>();
         destination = GameManager.Instance.Destination;
     }
 
@@ -16,11 +18,24 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            ResetVelocityTo(Vector3.zero);
+        }
+    }
+
     private void Move()
     {
         Vector3 dirToTarget = (destination.transform.position - transform.position).normalized;
         transform.Translate(dirToTarget * Time.deltaTime * playerData.MoveSpeed, Space.World);
         var destinationPos = new Vector3(destination.transform.position.x, 0f, destination.transform.position.z);
         transform.LookAt(destinationPos);
+    }
+
+    private void ResetVelocityTo(Vector3 newVeclocity)
+    {
+        body.velocity = newVeclocity;
     }
 }
