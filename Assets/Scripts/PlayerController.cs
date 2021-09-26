fee5f60d4a1a68d8265bc9c3c8ee9c83ad3fee4f
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-09-26 13:36:13 +0300

    Add Run Race 3D Asset , Update Player move logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] PlayerData playerData;
 
     private GameObject destination;
+    private Animator animator;
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         destination = GameManager.Instance.Destination;
     }
 
@@ -16,10 +18,19 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Destination"))
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
     private void Move()
     {
         Vector3 dirToTarget = (destination.transform.position - transform.position).normalized;
         transform.Translate(dirToTarget * Time.deltaTime * playerData.MoveSpeed, Space.World);
-        transform.LookAt(destination.transform.position);
+        var destinationPos = new Vector3(destination.transform.position.x, 0f, destination.transform.position.z);
+        transform.LookAt(destinationPos);
     }
 }
